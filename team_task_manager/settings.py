9a6572f55010e2df8718137784e4268e84d08ead
Yevhen <evgeniy.playid@icloud.com>
2024-03-20 09:36:44 +0200

    staticfiles managing

--- team_task_manager/settings.py
+++ team_task_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 import crispy_forms
 
@@ -32,7 +33,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = [
-    "localhost",
+    "localhost", "127.0.0.1",
 ]
 
 # Application definition
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -89,6 +91,11 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = "postgres://ybxfzvgk:yetCcLQqbLG4U2lFRVJJuVo79bE5L6dH@cornelius.db.elephantsql.com/ybxfzvgk"
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -123,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -155,6 +163,5 @@ LOGGING = {
 }
 
 
-DATABASE_URL = "postgres://ybxfzvgk:yetCcLQqbLG4U2lFRVJJuVo79bE5L6dH@cornelius.db.elephantsql.com/ybxfzvgk"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
